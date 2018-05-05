a9295b289040205419fdfd1008aa7a388fd422ee
vitor <vitor@vitors-iMac.local>
2018-05-05 17:14:10 +0200

    05/05

---
+++ mini.h
@@ -0,0 +1,13 @@
+#ifndef MINI_H
+# define MINI_H
+
+#include "libft/libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#endif
---
+++ vimrc
@@ -0,0 +1,10 @@
+set nu
+syntax on
+set mouse:a
+set ruler
+set smartindent
+set autoindent
+let c_space_errors=1
+set tabstop=4
+set softtabstop=0 noexpandtab
+set shiftwidth=4
